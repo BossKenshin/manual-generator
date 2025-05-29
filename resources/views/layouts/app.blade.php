e6e8b50d84affdfccdc065dc72ce48d854b7124f
Christian Lawrence Perino Rosales <r.christianlawrence13@gmail.com>
2025-05-30 06:43:37 +0800

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <title>Manuals</title>
     <link href="{{ asset('build/assets/app.css') }}" rel="stylesheet">
     <!-- <link rel="stylesheet" href="asset('resources/css/app.css')"> -->
-    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 
@@ -28,10 +28,10 @@
     </main>
 
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeo8XK0JzA2rm9eazj1pqcYjzJmGGzQ0zZXqA6Rw5r6q5zKK"
-        crossorigin="anonymous"
-    ></script>
+
+
+
+   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
+
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css',
+                 'resources/js/app.js',
+                         'resources/js/manualhead/manualhead.js', // 👈 Add this line
+
+
+            ],
             refresh: true,
         }),
         tailwindcss(),
