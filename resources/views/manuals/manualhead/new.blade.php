a9cb768dd8dbd02c204bf53e39c82b0f95e47a96
Christian Lawrence Perino Rosales <r.christianlawrence13@gmail.com>
2025-05-30 05:49:43 +0800

    dfd

--- resources/views/manuals/manualhead/new.blade.php
+++ resources/views/manuals/manualhead/new.blade.php
@@ -89,8 +89,7 @@
 
 
 
-        <!-- @vite('resources/js/manualhead/manualhead.js') -->
-         <script src="{{ asset('build/assets/manualhead.js') }}"></script>
+        <script src="{{ asset('build/assets/manualhead.js') }}"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
 
