7f637fd7788f153854cb4df83b0ad78ca7c46fd4
Christian Lawrence Perino Rosales <r.christianlawrence13@gmail.com>
2025-05-30 08:45:48 +0800

    fff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,23 +3,17 @@
 <head>
     <title>Manuals</title>
     <link href="{{ asset('build/assets/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('build/assets/app.js') }}"></script>
     <!-- <link rel="stylesheet" href="asset('resources/css/app.css')"> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 
-         <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous"
-    >
-
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
-            <a class="navbar-brand" href="#">Manual System</a>
+            <a class="navbar-brand" href="#">Manual Generator</a>
         </div>
     </nav>
 
